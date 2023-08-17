eee975a055deaf24e3e7760b1fb1d1ae48e47a43
bmwork <brenomartinsmb@gmail.com>
2023-08-17 16:53:58 -0300

    fixed level input allowing non numerical values

--- src/lib/components/Form/Input.svelte
+++ src/lib/components/Form/Input.svelte
@@ -1,14 +1,34 @@
 <script lang="ts">
 	export let value: string | number;
 	export let inputLabel: string;
+	export let isNumber: boolean = false;
+
+	const allowOnlyNumeric = (e: KeyboardEvent) => {
+		let numericals = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+		if (!numericals.includes(e.key)) {
+			e.preventDefault();
+		}
+	};
 </script>
 
 <label for={inputLabel} class="block font-bold text-sm sm:text-lg">{inputLabel}</label>
-<div class="mt-1 mb-8">
-	<input
-		id={inputLabel}
-		type="text"
-		class=" bg-primary-200 h-6 sm:h-fit border border-primary-100 focus:border-secondary-300 text-sm sm:text-lg focus:bg-primary-300 outline-none"
-		bind:value
-	/>
-</div>
+{#if !isNumber}
+	<div class="mt-1 mb-8">
+		<input
+			id={inputLabel}
+			type="text"
+			class=" bg-primary-200 h-6 sm:h-fit border border-primary-100 focus:border-secondary-300 text-sm sm:text-lg focus:bg-primary-300 outline-none"
+			bind:value
+		/>
+	</div>
+{:else}
+	<div class="mt-1 mb-8">
+		<input
+			on:keydown={allowOnlyNumeric}
+			id={inputLabel}
+			type="number"
+			class=" bg-primary-200 h-6 sm:h-fit border border-primary-100 focus:border-secondary-300 text-sm sm:text-lg focus:bg-primary-300 outline-none"
+			bind:value
+		/>
+	</div>
+{/if}
